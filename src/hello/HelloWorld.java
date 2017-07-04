a6d8263af7857c6b8f6a35031cadc264fddebc41
vignesh <vigneshdas3@gmail.com>
2017-07-04 12:08:46 +0530

    First Commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class HelloWorld {
+	
+	static void init(){
+		System.out.println("Heloo");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Hiiii");
+		System.out.println("Hiiii");*/
+		HelloWorld.init();
+	}
+
+}
---
+++ src/hello/HelloWorld1.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class HelloWorld1 {
+
+	static void init(){
+		System.out.println("Heloo0000000");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Hiiii");
+		System.out.println("Hiiii");*/
+		HelloWorld1.init();
+	}
+
+}
